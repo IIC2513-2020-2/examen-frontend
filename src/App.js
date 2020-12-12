861a1d218426ced261d20b23cd021bb23a937d38
Sebastián Vicencio <sivicencio@gmail.com>
2020-12-12 18:11:25 -0300

    feat(static): update basic layout and add static HTML files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import logo from './assets/solar-eclipse.jpg'
 
 function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <h1>Examen IIC2513 2020-2</h1>
+        <div className="brand">
+          <img src={logo} />
+          <h1>Examen IIC2513 2020-2</h1>
+        </div>
         <ul>
           <li>Username</li>
           <li><button>Salir</button></li>
